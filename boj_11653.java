d4a8b8d8f84d8ebf50e360cc1d42e1a15ed6776c
유예린 <irene4311@naver.com>
2024-07-10 14:52:07 +0900

    boj_11653

---
+++ boj_11653.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class boj_11653 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		
+		for(int i = 2; i <= n; i++) {
+			while(n % i == 0) {
+				System.out.println(i);
+				n /= i;
+			}
+		}
+	}
+}

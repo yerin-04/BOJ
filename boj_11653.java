ce79d97305972e7613f7415b8e34de1fcc9016c1
유예린 <irene4311@naver.com>
2024-07-10 14:52:51 +0900

    boj_11653

--- boj_11653.java
+++ boj_11653.java
@@ -5,7 +5,7 @@ public class boj_11653 {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
 		
-		for(int i = 2; i <= n; i++) {
+		for(int i = 2; i <= n; i++) { //for문 안에 while문 넣어 소인수분해 구현
 			while(n % i == 0) {
 				System.out.println(i);
 				n /= i;
